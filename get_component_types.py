5f113ccb8851b9b9b3d0a86ddbb2ca82debf6bc9
Sarah <sarah.graessle@gmail.com>
2023-07-21 16:10:53 +0200

    deleted some old, not needed files

--- get_component_types.py
+++
@@ -1,7 +0,0 @@
-def get_component_types(components):
-    component_types=[]
-    for i in range(len(components)):
-        type=components[i].type
-        if type not in component_types:
-            component_types.append(type)
-    return component_types
--- old_code/test_parse_xml_ET.py
+++
@@ -1,9 +0,0 @@
-from old_code.xml_to_dataframe import xml_to_dataframe
-
-
-filename = 'Sample_Data\model_matching_test_1.rexs'
-
-components_df,relations_df = xml_to_dataframe(filename)
-print(components_df)
-print(relations_df)
-
