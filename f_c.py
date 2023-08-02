ef1f5dfb73cfa24dc5892f4140474d86fa807901
Sarah Graessle <sarah.graessle@gmail.com>
2023-08-02 15:15:56 +0200

    modified for empty attribute lists

--- f_c.py
+++ f_c.py
@@ -14,10 +14,13 @@ def f_c(components, components_prime):
 
     for c in range(len_comp):
         for c_prime in range(len_comp_prime):
-            if components[c].type == components_prime[c_prime].type:
+            if (components[c].type == "concept_bearing" and components_prime[c_prime].type in ["concept_bearing", "rolling_bearing_with_catalog_geometry", "rolling_bearing_with_detailed_geometry", "slide_bearing"]) or (components[c].type == "rolling_bearing_with_catalog_geometry" and components_prime[c_prime].type in ["concept_bearing", "rolling_bearing_with_catalog_geometry", "rolling_bearing_with_detailed_geometry", "slide_bearing"]) or (components[c].type == "rolling_bearing_with_detailed_geometry" and components_prime[c_prime].type in ["concept_bearing", "rolling_bearing_with_catalog_geometry", "rolling_bearing_with_detailed_geometry"]) or (components[c].type == "slide_bearing" and components_prime[c_prime].type in ["concept_bearing", "slide_bearing"]) or (components[c].type in ["coupling", "switchable_coupling" ] and components_prime[c_prime].type in ["coupling", "switchable_coupling"]) or (components[c].type == components_prime[c_prime].type):
                 total_number_attributes = max(len(components[c].attributes), len(components_prime[c_prime].attributes))
-                if total_number_attributes == 0:
-                    f_c_matrix[c][c_prime] = 1 
+                if len(components[c].attributes) == 0 and len(components_prime[c_prime].attributes) == 0:
+                    f_c_matrix[c][c_prime] = 1
+                # TODO: empty list of attributes
+                elif min(len(components[c].attributes), len(components_prime[c_prime].attributes)) == 0:
+                    f_c_matrix[c][c_prime] = 0.1
                 else:
                     number_attributes_equal = 0
                     attributes = components[c].attributes
