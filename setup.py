511ce8930d97d5dc42b1827f003d44596cf224b9
douglas-fva <sarah.douglas@fva-service.de>
2023-09-11 11:01:19 +0200

    Setup for building python app to executables Builds for Linux, Windows and Mac executables in a separate "build" Folder At the moment, it rebuilds for every push-action at the repository. Other build-rhythms might be better suitable

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from cx_Freeze import setup, Executable
+
+buildOptions = dict(
+    include_files = [
+        'Sample_Data/'
+    ]
+)
+def find_data_file(filename):
+    if getattr(sys, 'frozen', False):
+        # The application is frozen
+        datadir = os.path.dirname(sys.executable)
+    else:
+        # The application is not frozen
+        # Change this bit to match where you store your data files:
+        datadir = os.path.dirname(__file__)
+
+    return os.path.join(datadir, filename)
+
+setup(
+    name = "Rexs Model Differention",
+    version = "1",
+    description = "",
+    options= {'build_exe': buildOptions},
+    executables = [
+        Executable(
+            script="main.py",
+            target_name="REXSMatching",
+            base=None
+        )
+    ]
+)
