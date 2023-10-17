175248313b94f504c5dcd787bc347503e8b04acd
douglas-fva <sarah.douglas@fva-service.de>
2023-10-17 13:24:00 +0200

    Mutability Error

--- np_ones_zeros.py
+++ np_ones_zeros.py
@@ -1,7 +1,7 @@
 
 def ones(countx: int, county:int): 
-    return [[1.]*county]*countx
+    return [[1. for _ in range(county)] for _ in range(countx)]
 
 def zeros(countx: int, county:int): 
-    return [[0.]*county]*countx
+    return [[0. for _ in range(county)] for _ in range(countx)]
     
